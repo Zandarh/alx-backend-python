3843c052c1b04c765349dbe5021435ce5e12bc25
Alexander Ubaka <ubaksxander@gmail.com>
2024-10-12 03:08:04 +0100

    Fllor function added

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+
+def floor(n : float) -> int:
+    """
+        Floor - Takes a flot and returns the floor of it
+        Parameter:
+        n : A float
+
+        Return : an int
+    """
+    return math.floor(n)
