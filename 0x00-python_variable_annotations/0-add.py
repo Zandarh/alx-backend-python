3d61edde6c9cbc4d744985cdbd89e392f8c2f439
Alexander Ubaka <ubaksxander@gmail.com>
2024-10-12 02:43:21 +0100

    Function add Created

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def add(a : float, b : float) -> float:
+    """
+        Add two integers together.
+        Parameters:
+        a (float): The first integer to be added
+        b (float): The second integer to be added
+        Returns:
+        float: The sum of a and b
+    """
+    return (a + b)
