7bb04375edb7742e97026898444f4730faf60f8c
Alexander Ubaka <ubaksxander@gmail.com>
2024-10-12 03:34:01 +0100

    Sum list done

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def sum_list(input_list: list[float]) -> float:
+    """
+        Sum_list : Add elements in list together
+        Parameter : List element.
+
+        Return: The sum in float
+    """
+    return sum(input_list)
