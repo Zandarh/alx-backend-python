66f82f1d2a26d6cf7b81fac2ab5e911e94d061d5
Alexander Ubaka <ubaksxander@gmail.com>
2024-10-12 03:14:40 +0100

    String Conversion

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str :
+    """
+        t0_str : Takes a float and returns the string version
+        Parameter:
+        n: The float
+
+        Return: A string
+    """
+    return str(n)
