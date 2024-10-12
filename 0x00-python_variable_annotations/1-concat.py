db28f8235e9629ecd8deab06022daf116b3e3eea
Alexander Ubaka <ubaksxander@gmail.com>
2024-10-12 02:54:12 +0100

    Concat function added

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def concat(str1 : str, str2 : str) -> str:
+    """
+        Typed Function that concatenates two strings
+        Parameters:
+        Str1 : First Parameter
+        Str2 : Second Str Parameter
+
+        Return:
+            A string of both strings
+    """
+    return (str1 + str2)
